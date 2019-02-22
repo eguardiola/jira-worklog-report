8269b00751306f4d88495ec264e3b803debef9e6
Guardiola Serrano, Eduardo <eguardiola@inaer.es>
2019-02-22 12:05:34 +0100

    Fixed to use official java jira client

--- src/main/java/es/bmcs/jiraworklogreport/WorkLog.java
+++ src/main/java/es/bmcs/jiraworklogreport/WorkLog.java
@@ -1,6 +1,6 @@
 package es.bmcs.jiraworklogreport;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.joda.time.DateTime;
 
 public class WorkLog {
